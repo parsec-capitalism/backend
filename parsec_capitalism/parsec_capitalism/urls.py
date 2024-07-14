d89d94a53a7b18beed2c9c9fba697441bd857529
cryosteam <g.korenblyum@gmail.com>
2024-07-14 22:47:26 +0100

    Change api waypoint

--- parsec_capitalism/parsec_capitalism/urls.py
+++ parsec_capitalism/parsec_capitalism/urls.py
@@ -1,12 +1,21 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path, include
+from rest_framework import routers
+
+from ships import views
+
+router = routers.DefaultRouter()
+router.register(r'ships', views.ShipViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
     path('ships/', include('ships.urls')),
     path('missions/', include('missions.urls')),
+    path('api/', include(router.urls)),
+    path('api-auth/', include(
+        'rest_framework.urls', namespace='rest_framework')),
 ]
 
 if settings.DEBUG:
--- parsec_capitalism/ships/urls.py
+++ parsec_capitalism/ships/urls.py
@@ -1,14 +1,9 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from . import views
 
 app_name = 'ships'
-router = routers.DefaultRouter()
-router.register(r'ships', views.ShipViewSet)
 
 urlpatterns = [
     path('', views.ships_list, name='ships_list'),
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
