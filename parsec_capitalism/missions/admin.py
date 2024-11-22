35dca1c76ed1a739074b00adaab99b9ae11be9ff
cryosteam <g.korenblyum@gmail.com>
2024-11-22 17:00:05 +0000

    feat: add Mission model

--- parsec_capitalism/missions/admin.py
+++ parsec_capitalism/missions/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
+
 from .models import Missions
 
 
 @admin.register(Missions)
 class MissionsAdmin(admin.ModelAdmin):
-    list_display = (
-        'title',
-    )
+    list_display = ('codename',)
--- parsec_capitalism/missions/models.py
+++ parsec_capitalism/missions/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 
 class Missions(models.Model):
-    title = models.CharField(max_length=128, verbose_name='Mission name')
+    codename = models.CharField('Codename', max_length=128)
+    expansion = models.CharField('Expansion', max_length=128)
+    reward = models.IntegerField('Reward')
+    summary = models.CharField('Summary', max_length=400)
+    duration = models.IntegerField('Duration')
+    distance = models.IntegerField('Distance')
+    volume = models.IntegerField('Volume')
+    weight = models.IntegerField('Weight')
 
     class Meta:
         verbose_name = 'Mission'
